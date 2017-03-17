c80a389be0b4967df6af9d110a486a4689bd833d
Josh Long <josh@joshlong.com>
2017-03-17 00:19:10 -0500

    got the gcp-service-broker app deployed to CF

--- src/main/java/stackdriver/deployer/StackdriverDeployerApplication.java
+++ src/main/java/stackdriver/deployer/StackdriverDeployerApplication.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.cloudfoundry.operations.CloudFoundryOperations;
 import org.cloudfoundry.operations.applications.*;
+import org.cloudfoundry.operations.serviceadmin.CreateServiceBrokerRequest;
 import org.cloudfoundry.operations.services.BindServiceInstanceRequest;
 import org.cloudfoundry.operations.services.CreateServiceInstanceRequest;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +14,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -42,7 +44,10 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 
 	private final CloudFoundryOperations cloudFoundryOperations;
 
-	public DeployerCommandLineRunner(CloudFoundryOperations cloudFoundryOperations, ManifestService manifestService, Sanitizer sanitizer) {
+	public DeployerCommandLineRunner(
+			CloudFoundryOperations cloudFoundryOperations,
+			ManifestService manifestService,
+			Sanitizer sanitizer) {
 		this.cloudFoundryOperations = cloudFoundryOperations;
 		this.manifestService = manifestService;
 		this.sanitizer = sanitizer;
@@ -51,12 +56,12 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 		String jsonPath = "/Users/jlong/Desktop/gcp.json";
-		String gcpServiceBrokerApplicationURL = deployGcpServiceBrokerApplication(jsonPath);
-		convertApplicationIntoServiceBroker(gcpServiceBrokerApplicationURL);
+		String gcpServiceBrokerApplicationURL = this.deployGcpServiceBrokerApplication(jsonPath);
+		this.convertApplicationIntoServiceBroker("gcp-service-broker", gcpServiceBrokerApplicationURL, "admin", "admin");
 		log.info("service broker application URL = " + gcpServiceBrokerApplicationURL);
-/*
+
 		String stackdriverProxyApplication = deployStackdriverProxyApplication();
-		log.info("stackdriverProxyApplication = " + stackdriverProxyApplication);*/
+		log.info("stackdriverProxyApplication = " + stackdriverProxyApplication);
 	}
 
 	private String deployGcpServiceBrokerApplication(String jsonPath) throws Exception {
@@ -64,17 +69,17 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 		String binaryName = "gcp-service-broker";
 		String manifestName = "manifest.yml";
 		String procfile = "Procfile";
-		Path uploadPath = stageTempUploadDir(inputUploadDirectory, binaryName, manifestName, procfile);
+		Path uploadPath = stageTemporaryUploadDir(inputUploadDirectory, binaryName, manifestName, procfile);
 		String planName = "512mb";
 		String serviceInstanceName = "gcp-service-broker-db";
-		String serviceName = "p-mysql";
+		String serviceTypeKey = "p-mysql";
 
-		this.provisionServiceInstance(planName, serviceInstanceName, serviceName);
+		this.provisionServiceInstance(serviceTypeKey, planName, serviceInstanceName);
 		ApplicationManifest manifest = this.manifestService.applicationManifestFrom(new File(uploadPath.toFile(), "manifest.yml").toPath());
 		this.pushApplicationUsingManifest(uploadPath, manifest, false);
 
 		ApplicationEnvironments environments = this.cloudFoundryOperations.applications().getEnvironments(GetApplicationEnvironmentsRequest.builder().name(manifest.getName()).build()).block();
-		Map<String, String> env = readDatabaseEnvironment(environments);
+		Map<String, String> env = readDatabaseEnvironment(environments, serviceTypeKey);
 		env.put("ROOT_SERVICE_ACCOUNT_JSON", Files.readAllLines(Paths.get(jsonPath))
 				.stream().collect(Collectors.joining(System.lineSeparator())));
 		env.forEach((k, v) -> cloudFoundryOperations.applications().setEnvironmentVariable(SetEnvironmentVariableApplicationRequest
@@ -84,28 +89,15 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 				.variableValue(v)
 				.build()).block());
 		this.cloudFoundryOperations.applications().start(StartApplicationRequest.builder().name(manifest.getName()).build()).block();
-		ApplicationDetail applicationDetail = this.cloudFoundryOperations.applications().get(GetApplicationRequest.builder().name(manifest.getName()).build()).block();
-		return applicationDetail.getUrls().get(0);
+		ApplicationDetail applicationDetail = this.cloudFoundryOperations.applications()
+				.get(GetApplicationRequest.builder().name(manifest.getName()).build())
+				.block();
+		return url(applicationDetail.getUrls().get(0));
 	}
 
-	@SuppressWarnings("unchecked")
-	private Map<String, String> readDatabaseEnvironment(ApplicationEnvironments environments) {
-		Map vcapServices = (Map) environments.getSystemProvided().get("VCAP_SERVICES");
-		List list = (List) vcapServices.get("p-mysql");
-		Map vars = (Map) list.get(0);
-		Map<String, Object> creds = (Map<String, Object>) vars.get("credentials");
-		Map<String, String> env = new HashMap<>();
-		env.put("DB_NAME", creds.get("name").toString());
-		env.put("DB_PORT", creds.get("port").toString());
-		env.put("DB_HOST", creds.get("hostname").toString());
-		env.put("DB_USERNAME", creds.get("username").toString());
-		env.put("DB_PASSWORD", creds.get("password").toString());
-		env.put("SECURITY_USER_NAME", "admin");
-		env.put("SECURITY_USER_PASSWORD", "admin");
-		return env;
-	}
-
-	private void provisionServiceInstance(String planName, String serviceInstanceName, String serviceName) {
+	private void provisionServiceInstance(String serviceName,
+	                                      String planName,
+	                                      String serviceInstanceName) {
 		this.cloudFoundryOperations.services()
 				.createInstance(CreateServiceInstanceRequest.builder()
 						.planName(planName)
@@ -115,69 +107,34 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 				.block();
 	}
 
-
-	private Path stageTempUploadDir(String inputUploadDirectory,
-	                                String binaryName,
-	                                String manifestName, String... others) throws IOException {
-		Resource rootGcpAssetsDirectory
-				= new ClassPathResource(inputUploadDirectory);
-		Assert.isTrue(rootGcpAssetsDirectory.exists(), "the directory containing the GCP assets should exist");
-
-		Resource binary = rootGcpAssetsDirectory.createRelative(binaryName);
-		Assert.isTrue(binary.exists(), "the gcp service broker binary should exist");
-
-		Resource manifest = rootGcpAssetsDirectory.createRelative(manifestName);
-		Assert.isTrue(manifest.exists(), "the manifest should exist");
-
-		File tmpDir = Files.createTempDirectory("staging").toFile();
-		log.debug("attempting to create a temporary staging directory " + tmpDir.getAbsolutePath());
-
-		Assert.isTrue(tmpDir.exists() || tmpDir.mkdirs(), "tried to create a staging directory");
-
-		copyResourceToFile(binary, new File(tmpDir, binaryName).toPath());
-		copyResourceToFile(manifest, new File(tmpDir, "manifest.yml").toPath());
-
-		for (String o : others) {
-			copyResourceToFile(rootGcpAssetsDirectory.createRelative(o), new File(tmpDir, o).toPath());
-		}
-
-		return tmpDir.toPath();
-	}
-
-
-	private static Path copyResourceToFile(Resource resource, Path targetPath) {
-		Assert.notNull(resource, "the resource must be non-null!");
-		Assert.isTrue(resource.exists(), "the resource " + resource.getFilename() + " must exist");
-		try (InputStream in = resource.getInputStream()) {
-			Files.copy(in, targetPath);
-			return targetPath;
-		} catch (IOException e) {
-			throw new RuntimeException("Exception while copying file to path: " + targetPath.toString(), e);
-		}
-	}
-
-
 	private String deployStackdriverProxyApplication() {
 
 
 		return null;
 	}
 
+	private void convertApplicationIntoServiceBroker(
+			String serviceBrokerName,
+			String stackdriverProxyApplicationUrl,
+			String usr, String pw) {
 
-	private void convertApplicationIntoServiceBroker(String stackdriverProxyApplication) {
-		/*
-		this.cloudFoundryOperations.serviceAdmin().create(CreateServiceBrokerRequest.builder()
-				.spaceScoped(true)
-				.url(stackdriverProxyApplication)
-				.name()
-				.username()
-				.password()
-				.build()).block();
-				*/
+		this.cloudFoundryOperations
+				.serviceAdmin()
+				.create(
+						CreateServiceBrokerRequest.builder()
+								.spaceScoped(true)
+								.url(stackdriverProxyApplicationUrl)
+								.name(serviceBrokerName)
+								.username(usr)
+								.password(pw)
+								.build())
+				.block();
+
 	}
 
 	public void pushApplicationUsingManifest(Path binaryFile,
-	                                         ApplicationManifest manifest, boolean start) {
+	                                         ApplicationManifest manifest,
+	                                         boolean start) {
 
 		PushApplicationRequest request = this.manifestService.fromApplicationManifest(binaryFile, manifest);
 		cloudFoundryOperations.applications().push(request).block();
@@ -192,8 +149,7 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 											BindServiceInstanceRequest.builder()
 													.applicationName(request.getName()).serviceInstanceName(svc)
 													.build()).block();
-							log.debug("bound service '" + svc + "' to '" + request.getName()
-									+ "'.");
+							log.debug("bound service '" + svc + "' to '" + request.getName() + "'.");
 						});
 			}
 			if (manifest.getEnvironmentVariables() != null) {
@@ -216,5 +172,65 @@ class DeployerCommandLineRunner implements CommandLineRunner {
 						.block();
 			}
 		}
+	}
+
+
+	private static Path copyResourceToFile(Resource resource, Path targetPath) {
+		Assert.notNull(resource, "the resource must be non-null!");
+		Assert.isTrue(resource.exists(), "the resource " + resource.getFilename() + " must exist");
+		try (InputStream in = resource.getInputStream()) {
+			Files.copy(in, targetPath);
+			return targetPath;
+		} catch (IOException e) {
+			throw new RuntimeException("Exception while copying file to path: " + targetPath.toString(), e);
+		}
+	}
+
+
+	@SuppressWarnings("unchecked")
+	private static Map<String, String> readDatabaseEnvironment(ApplicationEnvironments environments, String serviceTypeKey) {
+		Map<String, Object> vcapServices = (Map<String, Object>) environments.getSystemProvided().get("VCAP_SERVICES");
+		List<Object> list = (List<Object>) vcapServices.get(serviceTypeKey);
+		Map<String, Object> vars = (Map<String, Object>) list.get(0);
+		Map<String, Object> credentials = (Map<String, Object>) vars.get("credentials");
+		Map<String, String> env = new HashMap<>();
+		env.put("DB_NAME", credentials.get("name").toString());
+		env.put("DB_PORT", credentials.get("port").toString());
+		env.put("DB_HOST", credentials.get("hostname").toString());
+		env.put("DB_USERNAME", credentials.get("username").toString());
+		env.put("DB_PASSWORD", credentials.get("password").toString());
+		env.put("SECURITY_USER_NAME", "admin");
+		env.put("SECURITY_USER_PASSWORD", "admin");
+		return env;
+	}
+
+	private static Path stageTemporaryUploadDir(String inputUploadDirectory,
+	                                            String binaryName,
+	                                            String manifestName,
+	                                            String... others) throws IOException {
+		Resource rootGcpAssetsDirectory = new ClassPathResource(inputUploadDirectory);
+		Assert.isTrue(rootGcpAssetsDirectory.exists(), "the directory containing the GCP assets should exist");
+
+		Resource binary = rootGcpAssetsDirectory.createRelative(binaryName);
+		Assert.isTrue(binary.exists(), "the gcp service broker binary should exist");
+
+		Resource manifest = rootGcpAssetsDirectory.createRelative(manifestName);
+		Assert.isTrue(manifest.exists(), "the manifest should exist");
+
+		File tmpDir = Files.createTempDirectory("staging").toFile();
+		Assert.isTrue(tmpDir.exists() || tmpDir.mkdirs(), "tried to create a staging directory");
+
+		copyResourceToFile(binary, new File(tmpDir, binaryName).toPath());
+		copyResourceToFile(manifest, new File(tmpDir, "manifest.yml").toPath());
+
+		for (String o : others) {
+			copyResourceToFile(rootGcpAssetsDirectory.createRelative(o), new File(tmpDir, o).toPath());
+		}
+
+		return tmpDir.toPath();
+	}
+
+	private static String url(String u) {
+		return StringUtils.hasText(u) && u.toLowerCase().startsWith("http") ? u : "http://" + u;
 	}
 }
