4f552388590b579f1aa2049bd8defc00d5d6a02c
Josh Long <josh@joshlong.com>
2017-03-17 13:27:14 -0500

    updated

---
+++ synchronize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo CF_GCP_ROOT is $CF_GCP_ROOT
+echo "Synchronizing GCP Service Broker Asssets"
+cp $CF_GCP_ROOT/gcp/src/gcp-service-broker/manifest-linux-binary.yml $CF_GCP_ROOT/stackdriver-trace-deployer/src/main/resources/gcp-service-broker-assets/manifest.yml
+target_gcp_binary=$CF_GCP_ROOT/stackdriver-trace-deployer/src/main/resources/gcp-service-broker-assets/gcp-service-broker
+rm -rf $target_gcp_binary
+cp $CF_GCP_ROOT/gcp/bin/gcp-service-broker $target_gcp_binary
+
+
+echo "Synchronizing Stackdriver Zipkin Proxy Assets"
+cp $CF_GCP_ROOT/stackdriver-zipkin-cf/manifest.yml  $CF_GCP_ROOT/stackdriver-trace-deployer/src/main/resources/stackdriver-proxy-assets/manifest.yml
+cp $CF_GCP_ROOT/stackdriver-zipkin-cf/target/stackdriver-zipkin-cf.jar  $CF_GCP_ROOT/stackdriver-trace-deployer/src/main/resources/stackdriver-proxy-assets/proxy.jar
